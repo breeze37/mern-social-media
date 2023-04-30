b670ca521bab07b0733bd57095007be37a02b653
Arnab <74238471+breeze37@users.noreply.github.com>
2023-04-30 15:32:46 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,10 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
 
-function App() {
-  const mode = useSelector((state) => state.mode);
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-  const isAuth = Boolean(useSelector((state) => state.token));
+// function App() {
+//   const mode = useSelector((state) => state.mode);
+//   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+//   const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="app">
